e21685e5fc5ebd6d1b28d3b73d83bd185b66e090
manishwaran <manishwaran.np@gmail.com>
2016-12-02 19:10:00 +0530

    Convention changes.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ import CssOptimumSelectorHelper from './css-traversal-helper'
 import $ from 'jquery'
 export default class CssOptimumSelector extends CssOptimumSelectorHelper {
 
-  constructor(props) {
-    super(props)
-    this.cssPath = this.cssPath.bind(this)
-    this.uniqueCssSelector = this.uniqueCssSelector.bind(this)
-    this.getCommonSelector = this.getCommonSelector.bind(this)
-  }
-
-  cssPath(element, path = '') {
+  getUniqueCssSelector(element, path = '') {
     if (path && this.isUniqueInDocument(element, path)) return path
     const checkList = this.getCheckList(element)
     const filteredCheckList = this.getFilteredCheckList(checkList)
@@ -22,21 +15,24 @@ export default class CssOptimumSelector extends CssOptimumSelectorHelper {
       parentPath = `${this.nthChildStr(element)}`
       if (path) parentPath = `${parentPath} > ${path}`
     }
-    return this.cssPath(element.parent(), parentPath)
+    return this.getUniqueCssSelector(element.parent(), parentPath)
   }
 
   uniqueCssSelector(element) {
-    return this.cssPath($(element))
+    return this.getUniqueCssSelector($(element))
   }
 
-  getCommonSelector(firstElement, secondElement, relativeDepth = this.relativeDepth) {
+  getMultiSelector(firstElement, secondElement, relativeDepth = this.relativeDepth) {
     const commonParent = this.checkCommonParent(firstElement, secondElement)
-    const noCommonSelector = () => [this.cssPath(firstElement), this.cssPath(secondElement)]
+    const noCommonSelector = () => [
+      this.getUniqueCssSelector(firstElement),
+      this.getUniqueCssSelector(secondElement)
+    ]
     if (!commonParent) return noCommonSelector()
     const ele1Path = this.childToParentTraversal(firstElement, commonParent).slice(1).join(' > ')
     const ele2Path = this.childToParentTraversal(secondElement, commonParent).slice(1).join(' > ')
     if (this.checkCommonPath(ele1Path, ele2Path)) {
-      const path = this.cssPath(commonParent)
+      const path = this.getUniqueCssSelector(commonParent)
       const relativeDegree = this.checkRelativeDegree(ele1Path, ele2Path)
       const relativeChildPath = this.getRelativeChildPath(ele1Path,relativeDepth)
       const commonSelector = `${path} ${relativeChildPath}`
@@ -45,7 +41,7 @@ export default class CssOptimumSelector extends CssOptimumSelectorHelper {
     return noCommonSelector()
   }
 
-  commonSelector(firstElement, secondElement) {
-    return getCommonSelector($(firstElement), $(secondElement))
+  multiSelector(firstElement, secondElement, relativeDepth = this.relativeDepth) {
+    return getMultiSelector($(firstElement), $(secondElement))
   }
 }
